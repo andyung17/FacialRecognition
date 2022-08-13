3527cb3105f6a2bc840c419f1fbd9e409ed7e334
Andy Ung <43015964+andyung17@users.noreply.github.com>
2022-08-12 23:34:20 -0400

    Making code more readable

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-from email.mime import image
 import face_recognition
 import os
 from dotenv import load_dotenv
@@ -16,16 +15,20 @@ def scanDir():
 
 # Function for scanning all faces in an image
 def scanFaces(imageName):
+
+    # Reference image to compare against
     picture_of_me = face_recognition.load_image_file(os.getcwd() + "/images/" + os.getenv('REF_IMAGE'))
     my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
     imageName2 = imageName
+
     # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
     try:
         unknown_picture = face_recognition.load_image_file(os.getcwd() + "/images/" + imageName2)
         unknown_face = face_recognition.face_locations(unknown_picture)
+
         # Now we can see the two face encodings are of the same person with `compare_faces`!
         for face_location in unknown_face:
-            print(imageName)
+            # Declaring image to compare with
             unknownFacePresent = face_recognition.face_encodings(unknown_picture, known_face_locations = [face_location])[0]
             results = face_recognition.compare_faces([my_face_encoding], unknownFacePresent)
             if results[0] == True and imageName2 != os.getenv('REF_IMAGE'):
