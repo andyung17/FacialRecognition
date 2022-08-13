da76bc4e2918e0b1b9e6456581ad000f45e0fcc3
Andy Ung <andyung123456789@gmail.com>
2022-08-12 20:16:15 -0400

    basis for reading entire directory

--- script.py
+++ script.py
@@ -16,6 +16,7 @@ def scanFaces():
     my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
 
 imageName2 = './andy.jpg'
+# Test
 
 # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
 
